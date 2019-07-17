61152e8e74675f6f6ee74052ea9081eb20c633bc
Bernie Reiter <ockham@raz.or.at>
2019-07-17 14:48:53 +0600

    UpgradeNudge: import warning from '@wordpress/block-editor'

--- extensions/shared/upgrade-nudge/index.jsx
+++ extensions/shared/upgrade-nudge/index.jsx
@@ -7,7 +7,7 @@ import { Button } from '@wordpress/components';
 import { compact, get, startsWith } from 'lodash';
 import { compose } from '@wordpress/compose';
 import { withDispatch, withSelect } from '@wordpress/data';
-import { Warning } from '@wordpress/editor';
+import { Warning } from '@wordpress/block-editor';
 import Gridicon from 'gridicons';
 
 /**
